e1f79e7fc01a799635771c7fcde440b3dffd9ff0
Lee seung woo <USER@DESKTOP-8MRR329DHJ>
2024-07-17 19:41:02 +0900

    MySQL DB 연동하고

---
+++ GitTest/src/main/java/com/aischool/model/User.java
@@ -0,0 +1,80 @@
+package com.aischool.model;
+
+import java.security.Timestamp;
+
+public class User {
+	private String id;
+	private String pw;
+	private String email;
+	private String nick;
+	private String profile_img;
+	private char exit;
+	private Timestamp joindate;
+	private String type;
+	
+	public User() {};
+	
+	public User(String id, String pw, String email, String nick, String profile_img, char exit, Timestamp joindate,
+			String type) {
+		super();
+		this.id = id;
+		this.pw = pw;
+		this.email = email;
+		this.nick = nick;
+		this.profile_img = profile_img;
+		this.exit = exit;
+		this.joindate = joindate;
+		this.type = type;
+	}
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	public String getProfile_img() {
+		return profile_img;
+	}
+	public void setProfile_img(String profile_img) {
+		this.profile_img = profile_img;
+	}
+	public char getExit() {
+		return exit;
+	}
+	public void setExit(char exit) {
+		this.exit = exit;
+	}
+	public Timestamp getJoindate() {
+		return joindate;
+	}
+	public void setJoindate(Timestamp joindate) {
+		this.joindate = joindate;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
--- GitTest/src/main/java/com/aischool/service/TestService.java
+++ GitTest/src/main/java/com/aischool/service/TestService.java
@@ -1,11 +1,14 @@
 package com.aischool.service;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.aischool.model.UserDAO;
 
 
 @WebServlet("/TestService")
@@ -15,6 +18,10 @@ public class TestService extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("첫 번째");
 		System.out.println("두 번째 코드 생성");
+		
+		UserDAO dao = new UserDAO();
+		dao.connect();
+		dao.close();
 		
 	}
 
